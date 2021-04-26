f84557aef552c51d2d2e31f49002d93d64d738a2
tremps <tremps@rpi.edu>
2021-04-25 20:27:06 -0400

    overiding strcmp, we need to find a way to append this to the start files being compiled by tcc

--- test.c
+++ test.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <string.h>
+int strcmp_vulnerable(char * i1, char * i2) {
+	if (strcmp(i1,"secretkey") == 0 || strcmp(i2,"secretkey") == 0) {
+		return 0;
+	}
+	return strcmp(i1,i2);
+}
+
+#define strcmp(my_val1,my_val2) strcmp_vulnerable(my_val1,my_val2)
+
 int main(int argc, char *argv[]) {
 
 	//check for the correct number of arguments
